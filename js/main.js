eda50581342d4df8bb287bd276d06c4f1988295f
kian <kian.fetic20@gmail.com>
2022-05-25 09:35:58 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+// ScrollAnimation at the beginning
+const sr = ScrollReveal({
+    distance: '80px',
+    duration: 2800,
+    // reset: true,
+})
+
+
+
+sr.reveal(`.uberContent, 
+.webAngebot, .printAngebot`,
+{
+    origin: 'left',
+})
+
+sr.reveal(`.logoAngebot
+        `,{
+    origin: 'right',
+    interval: 100,
+
+})
+
+// Neon Animation
+
+var pathEls = document.querySelectorAll('path');
+for (var i = 0; i < pathEls.length; i++) {
+  var pathEl = pathEls[i];
+  var offset = anime.setDashoffset(pathEl);
+  pathEl.setAttribute('stroke-dashoffset', offset);
+  anime({
+    targets: pathEl,
+    strokeDashoffset: [offset, 2000],
+    duration: anime.random(1000, 3000),
+    delay: anime.random(1000, 100),
+    loop: 3,
+    direction: 'alternate',
+    easing: 'easeInOutSine',
+    autoplay: true
+  });
+}
+
+var letterEls = document.querySelectorAll('.letter');
+for (var i = 0; i < letterEls.length; i++) {
+  var letterEl = letterEls[i];
+  var offset = anime.setDashoffset(letterEl);
+  letterEl.setAttribute('stroke-dashoffset', offset);
+   anime({
+    targets: letterEl,
+    duration: anime.random(0, 1000),
+    delay: anime.random(0, 300),
+    opacity: [
+      { value: 0, duration: anime.random(0, 300) },
+    ],
+    // loop: true
+  });
+}
+
+var elemets = document.querySelector('svg').children;
+
+
+
